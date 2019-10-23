10d3231a83ce849b9452dd3b3f219920e5c7d0c2
RikeshThapa <rthapa@u.rochester.edu>
2019-10-23 11:37:55 -0400

    Setting up for recursive integer multiplication and Karatsuba

---
+++ Algorithms/Rcursive_Integer_Multiplication.py
@@ -0,0 +1,43 @@
+"""
+> Implementation of integer multiplication algorithm
+> Restriction to only multiplying by single digits
+> Options for implentation:
+>> Grade school long hand multiplication
+>> Karatsuba's Algorithm
+>> recursive integer multiplication
+>> good explanation and learning source here: https://www.cs.umd.edu/class/spring2016/cmsc351-0101/int_mult.pdf
+"""
+
+class product_calculator():
+    '''
+    Implement product multiplication.
+    1) Karatsuba
+    2) Recursive integer multiplication
+    3) gradeschool math implementation
+    '''
+    def karatsuba(a, b):
+        '''
+        1) convert A and B to strings
+        2) check string length
+        3) if string length is 1, sort and combine
+        4) if string length >1
+        5) call function again on smaller pairs
+        '''
+        return None
+
+    def recursive_integer_multiplication(a,b):
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    testA = 3141592653589793238462643383279502884197169399375105820974944592
+    testB = 2718281828459045235360287471352662497757247093699959574966967627
+    return int_product(testA, testB)
